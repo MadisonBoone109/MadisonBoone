a4df479f3c7da7e058bf1079af851a0236a19518
MadisonBoone109 <87313571+W046871822@users.noreply.github.com>
2024-02-05 13:29:58 -0400

    Add files via upload

---
+++ listMessages.php
@@ -0,0 +1,47 @@
+<?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>ID</th><th>Name</th><th>Message</th><th>Email</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
+
+$servername = "localhost";
+$username = "id21779421_phpuser";
+$password = "ContactForm1!";
+$dbname = "id21779421_contactform";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT id, name, message, email FROM messages");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
